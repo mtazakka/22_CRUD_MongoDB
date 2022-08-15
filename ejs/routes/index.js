983b02a68c336871411e7843dc6d69edf644efdc
tazakka <mymac@Jacks-MacBook-Pro.local>
2022-08-15 22:07:11 +0700

    proses

--- ejs/routes/index.js
+++ ejs/routes/index.js
@@ -1,9 +1,193 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function (db) {
+  const collection = db.collection('breads');
+  router.get('/', async (req, res) => {
+    const page = req.query.page || 1
+    const previous = (parseInt(page) - 1) == 0 ? 1 : (parseInt(page) - 1)
+    const next = parseInt(page) + 1
+    const limit = 3
+    const offset = parseInt((page - 1) * limit)
+    const url = req.url == '/' ? '/?page=1' : req.url
+    var sortBy = req.query.sortBy == undefined ? "_id" : req.query.sortBy
+    var order = req.query.order == undefined ? 1 : req.query.order
 
-module.exports = router;
+    var paramsSort = `{
+      "${sortBy}" : ${order}
+    }`
+    paramsSort = JSON.parse(paramsSort)
+
+    //searching
+    // const params = []
+
+    if (req.query.string && req.query.stringFilters == 'on') {
+      var paramsString = {
+        string:{'$regex' : req.query.string, '$options' : 'i'}
+        
+      }
+      // params.push(`string : /${req.query.string}/`)
+    }
+    if (req.query.integer && req.query.integerFilters == 'on') {
+      var paramsInteger = {
+        integer: {'$regex' : req.query.integer, '$options' : 'i'}
+      }
+      // params.push(`integer : /${req.query.integer}/`)
+    }
+    if (req.query.float && req.query.floatFilters == 'on') {
+      var paramsFloat = {
+        float: {'$regex' : req.query.float, '$options' : 'i'}
+      }
+      // params.push(`float : /${req.query.float}/`)
+    }
+    if (req.query.dateFilters == 'on') {
+      if (req.query.startDate != '' && req.query.toDate != '') {
+        var paramsDate = {
+        date: {"$gte": req.query.startDate, "$lte":req.query.toDate}
+        }
+        // paramsDate = JSON.parse(paramsDate)
+        // params.push(`date: {$gt: ${req.query.startDate}, $lt: ${req.query.toDate}}`)
+      } else if (req.query.startDate) {
+        const dated = moment(req.query.startdate).format('DD MMMM YYYY')
+        var paramsStartDate = {
+          'date' : {$gt: new Date(dated)}
+
+          // startDate: `new Date(${req.query.startDate})`
+        }
+        // params.push(`date: {$gt: ${req.query.startDate}}`)
+      } else {
+        var paramsToDate = {
+          toDate: `new Date ${req.query.toDate}`
+        }
+        // params.push(`date: {$lt: ${req.query.toDate}}`)
+      }
+    }
+    if (req.query.boolean && req.query.booleanFilters == 'on') {
+      var paramsBoolean = {
+        boolean: req.query.boolean
+      }
+      }
+      // params.push(`boolean = ${req.query.boolean}`)
+    
+    // console.log('1')
+    // console.log(params.join())
+
+    // console.log(limit)
+    // console.log(offset)
+    // try {
+    //   const findResult = await collection.find({ params }).toArray();
+    //   console.log('Found documents =>', findResult);
+    //   const total = findResult.rows[0].total
+    //   const pages = Math.ceil(total / limit)
+
+    try {
+    
+      // const finalParams = Object.assign(paramsString, paramsInteger, paramsFloat, paramsDate, paramsStartDate, paramsToDate, paramsBoolean)
+      const finalParams = {...paramsString, ...paramsInteger, ...paramsFloat, ...paramsDate, ...paramsStartDate, ...paramsToDate, ...paramsBoolean}
+      const findResult = await collection.find(finalParams).toArray()
+      console.log (findResult.length)
+      const pages = Math.ceil(findResult.length / limit)
+      // console.log('find parameter')
+      // console.log(finalParams)
+      // console.log(paramsSort)
+      
+      const lastResult = await collection.find(finalParams).skip(offset).limit(limit).sort(paramsSort).toArray()
+      
+      console.log(sortBy)
+      console.log(order)
+      console.log('Found documents =>', lastResult);
+      res.render('list', { rows: lastResult, data: lastResult, page, pages, previous, next, query: req.query, url, sortBy, order })
+    } catch (e) {
+      console.log(e)
+      console.log("Failed to Read")
+    }
+    // // } catch (e) {
+    // //   console.log(e)
+    // //   console.log("Failed to Read")
+    // }
+
+  })
+
+  //------------------Add--------------------//
+  router.get('/add', function (req, res, next) {
+    res.render('form')
+  })
+
+  router.post('/add', async function (req, res, next) {
+
+    try {
+      const dated = moment(req.query.date).format('DD MMMM YYYY')
+      const insertResult = await collection.insertOne(
+        {
+          string: req.body.string,
+          integer: req.body.integer,
+          float: req.body.float,
+          date: dated,
+          boolean: req.body.boolean
+        });
+      console.log('Inserted documents =>', insertResult);
+      res.redirect('/')
+    } catch (e) {
+      console.log(e)
+      console.log("Failed to Add")
+    }
+  })
+
+
+  //----------------Delete---------------//
+
+  router.get('/delete/:id', async (req, res) => {
+
+    try {
+      const index = req.params._id
+      console.log('Ini disini')
+      console.log(index)
+      const deleteResult = await collection.deleteOne({ id: index });
+      console.log('Deleted documents =>', deleteResult)
+    } catch (e) {
+      console.log(e)
+      console.log("Failed to Delete")
+    }
+    res.redirect('/')
+  })
+
+
+  //----------------Edit---------------//
+  router.get('/edit/:id', async (req, res) => {
+    try {
+      const index = req.params._id
+      const filteredDocs = await collection.find({ id: index }).toArray();
+      console.log(`Found documents filtered by { id: ${index} } =>`, filteredDocs);
+      res.render('edit', { item: filteredDocs[0], index })   /* res render = menerima dari file */
+    } catch (e) {
+      console.log(e)
+      console.log("Failed to Delete")
+    }
+  })
+
+  router.post('/edit/:id', async (req, res) => {
+    try {
+      const index = req.params._id
+      const dated = moment(req.query.date).format('DD MMMM YYYY')
+      const updateResult = await collection.updateOne({ id: index }, {
+        $set: {
+          string: req.body.string,
+          integer: req.body.integer,
+          float: req.body.float,
+          date: dated,
+          boolean: req.body.boolean
+        }
+      });
+      console.log('Updated documents =>', updateResult);
+      res.redirect('/')
+    } catch (e) {
+      console.log(e)
+      console.log("Failed to Edit")
+    }
+  })
+
+  return router;
+}
+
+
--- ejs/routes/users.js
+++ ejs/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+module.exports = function(db){
+  // const collection = db.collection('users');
+router.get('/', function (req, res, next) {
+  db.query('db.users.find()', (err, data)=>{
+    if(err) return res.send (err)
+res.send('list', {data:data.rows})
+  })
+
+  // res.send('respond with a resource');
 });
 
-module.exports = router;
+ return router;
+}
