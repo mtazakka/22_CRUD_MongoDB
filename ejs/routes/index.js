74b3b0457bdc8d990d01fa1bb0b82aea7326b165
tazakka <mymac@Jacks-MacBook-Pro.local>
2022-08-16 09:37:17 +0700

    16 Aug

--- ejs/routes/index.js
+++ ejs/routes/index.js
@@ -8,7 +8,7 @@ module.exports = function (db) {
     const page = req.query.page || 1
     const previous = (parseInt(page) - 1) == 0 ? 1 : (parseInt(page) - 1)
     const next = parseInt(page) + 1
-    const limit = 3
+    const limit = 5
     const offset = parseInt((page - 1) * limit)
     const url = req.url == '/' ? '/?page=1' : req.url
     var sortBy = req.query.sortBy == undefined ? "_id" : req.query.sortBy
@@ -44,14 +44,14 @@ module.exports = function (db) {
     if (req.query.dateFilters == 'on') {
       if (req.query.startDate != '' && req.query.toDate != '') {
         var paramsDate = {
-        date: {"$gte": req.query.startDate, "$lte":req.query.toDate}
+        // date: {$gte: {'$regex': new Date(req.query.startDate), '$options' : 'i'}, $lte:{'$regex': new Date(req.query.toDate), '$options' : 'i'}}
+        date: {$gte: new Date(req.query.startDate), $lte: new Date(req.query.toDate)}
         }
         // paramsDate = JSON.parse(paramsDate)
-        // params.push(`date: {$gt: ${req.query.startDate}, $lt: ${req.query.toDate}}`)
+        
       } else if (req.query.startDate) {
-        const dated = moment(req.query.startdate).format('DD MMMM YYYY')
         var paramsStartDate = {
-          'date' : {$gt: new Date(dated)}
+          'date' : {$gte: new Date(req.query.startDate)}
 
           // startDate: `new Date(${req.query.startDate})`
         }
@@ -86,10 +86,10 @@ module.exports = function (db) {
       // const finalParams = Object.assign(paramsString, paramsInteger, paramsFloat, paramsDate, paramsStartDate, paramsToDate, paramsBoolean)
       const finalParams = {...paramsString, ...paramsInteger, ...paramsFloat, ...paramsDate, ...paramsStartDate, ...paramsToDate, ...paramsBoolean}
       const findResult = await collection.find(finalParams).toArray()
-      console.log (findResult.length)
+      // console.log (findResult.length)
       const pages = Math.ceil(findResult.length / limit)
       // console.log('find parameter')
-      // console.log(finalParams)
+      console.log(finalParams)
       // console.log(paramsSort)
       
       const lastResult = await collection.find(finalParams).skip(offset).limit(limit).sort(paramsSort).toArray()
@@ -117,13 +117,14 @@ module.exports = function (db) {
   router.post('/add', async function (req, res, next) {
 
     try {
-      const dated = moment(req.query.date).format('DD MMMM YYYY')
+      // const dated = moment(req.body.date).format('DD MMMM YYYY')
       const insertResult = await collection.insertOne(
         {
+          id: req.body.id,
           string: req.body.string,
           integer: req.body.integer,
           float: req.body.float,
-          date: dated,
+          date: new Date(req.body.date),
           boolean: req.body.boolean
         });
       console.log('Inserted documents =>', insertResult);
@@ -140,7 +141,7 @@ module.exports = function (db) {
   router.get('/delete/:id', async (req, res) => {
 
     try {
-      const index = req.params._id
+      const index = req.params.id
       console.log('Ini disini')
       console.log(index)
       const deleteResult = await collection.deleteOne({ id: index });
@@ -156,26 +157,27 @@ module.exports = function (db) {
   //----------------Edit---------------//
   router.get('/edit/:id', async (req, res) => {
     try {
-      const index = req.params._id
+      const index = req.params.id
+      console.log(index)
       const filteredDocs = await collection.find({ id: index }).toArray();
       console.log(`Found documents filtered by { id: ${index} } =>`, filteredDocs);
       res.render('edit', { item: filteredDocs[0], index })   /* res render = menerima dari file */
     } catch (e) {
       console.log(e)
-      console.log("Failed to Delete")
+      console.log("Failed to Edit")
     }
   })
 
   router.post('/edit/:id', async (req, res) => {
     try {
-      const index = req.params._id
+      const index = req.params.id
       const dated = moment(req.query.date).format('DD MMMM YYYY')
       const updateResult = await collection.updateOne({ id: index }, {
         $set: {
           string: req.body.string,
           integer: req.body.integer,
           float: req.body.float,
-          date: dated,
+          date: new Date(req.body.date),
           boolean: req.body.boolean
         }
       });
